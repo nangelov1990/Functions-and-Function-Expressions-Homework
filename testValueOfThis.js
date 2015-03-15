1e52f300396bc1c2f6e2364cb0bf4a43666b76bb
Nick Angelov <nangelov1990@gmail.com>
2015-03-15 13:43:41 +0200

    Partial cleanup

--- testValueOfThis.js
+++ testValueOfThis.js
@@ -2,21 +2,16 @@
 
 function testContext() {
 	console.log(this);
-	// return this;
 }
 
 document.getElementById("testValueOfThis1").innerHTML += "From the global scope => " + testContext() + '<br>';
-testContext();
-// console.log(testContext());
+testContext.call(this);
 
 (function sampleFunction () {
-	var outputOfTestContext = testContext();
+	var outputOfTestContext = testContext.call(this);
 	document.getElementById("testValueOfThis1").innerHTML += "Inside another function => " + outputOfTestContext + '<br>';
 	testContext();
-	// console.log(testContext());
 })();
 
 var obj = new testContext();
 document.getElementById("testValueOfThis1").innerHTML += "As an object => " + obj + '<br>';
-
-// console.log(obj);
